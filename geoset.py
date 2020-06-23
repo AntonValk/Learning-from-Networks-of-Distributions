57daa5bdb658408e8d00f46a5765c9f6410c47b6
Antonios Valkanas <30447660+AntonValk@users.noreply.github.com>
2020-06-23 01:53:06 -0400

    Add files via upload

---
+++ geoset.py
@@ -0,0 +1,50 @@
+import geopandas as gpd
+from geopandas import GeoSeries
+from shapely.geometry import Polygon
+import shapely.geometry as geoms
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt 
+from preprocess import preprocess_data_1
+
+
+train_X, test_X, train_y, test_y = preprocess_data_1()
+
+
+nybb_path = gpd.datasets.get_path('nybb')
+boros = gpd.read_file(nybb_path)
+boros.set_index('BoroCode', inplace=True)
+boros.sort_index(inplace=True)
+# print(boros.crs)
+boros = boros.to_crs(epsg=4326)
+print(boros.head())
+boros.plot()
+plt.show()
+
+
+geos = gpd.GeoSeries(train_X[['longitude', 'latitude']]\
+            .apply(lambda x: geoms.Point((x.longitude, x.latitude)), axis=1), \
+            crs={'init': 'epsg:4326'})
+# geos = geos.to_crs(epsg=2263)
+
+# geos = gpd.GeoDataFrame(geos, geometry=gpd.points_from_xy(geos.longitude, geos.latitude))
+
+gdf = gpd.GeoDataFrame(train_X, geometry=geos)
+
+
+print(geos)
+
+geos.plot()
+plt.show()
+
+# db['geometry'] = geos
+# db = gpd.GeoDataFrame.from_records(db)
+# db.crs = geos.crs
+
+# only keep nyc 
+in_nyc = gpd.sjoin(gdf, boros, how="inner", op='intersects')
+# in_nyc = boros.contains(geos)
+
+print(list(in_nyc))
+print(in_nyc['BoroName'])
+# print(in_nyc.value_counts())
